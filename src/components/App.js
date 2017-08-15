84435cd3577b8e78a3c1ff35eeaf23d8f461aada
“Diljeet <jeetds1994@gmail.com>
2017-08-14 22:58:50 -0400

    still working

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
 import FruitBasket from './FruitBasket';
+import Filter from './Filter'
 
-const App = () => <FruitBasket />;
+class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      data: []
+    }
+  }
+
+  fetchFilters = () => {
+    fetch('/api/fruit_types')
+      .then(response => response.json())
+      .then(res => this.setState({
+         data: res
+      })
+      .then((data) => {
+        debugger
+      })
+    )
+  }
+
+  componentWillMount(){
+    this.fetchFilters()
+  }
+
+  render(){
+    return(
+    <div>
+      <Filter data={this.state.data}/>
+      <FruitBasket />;
+    </div>
+    )
+  }
+}
+
 
 export default App;
